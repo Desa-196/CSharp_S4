30b3e8ea69d9db007b2b2731f00279bafd6945a0
Пономарев Денис <desa-196@yandex.ru>
2024-03-07 09:35:20 +0300

    Решил задачу

---
+++ CSharp_S4/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = { 12, 2, 6, 11, 18, 45, 32, 9, 14, 64, 76, 33, 16, 5 };
+
+int target = 185;
+
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    var h = new HashSet<int>();
+
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (h.Contains(target - array[i] - array[j]))
+        {
+            Console.WriteLine($"{array[i]} + {target - array[i] - array[j]} +  {array[j]} = {target - array[i] - array[j] + array[i] + array[j]}");
+        }
+        else
+        {
+            h.Add(array[j]);
+        }
+    }
+}
